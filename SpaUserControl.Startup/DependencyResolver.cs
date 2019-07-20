4f333a972affe82e7736e5ea75a74275286a3610
Leonardo Davinc Silva <davinc131@hotmail.com>
2019-07-20 00:27:31 -0300

    Implementação do projeto de "Injeção de dependência" do projeto.

---
+++ SpaUserControl.Startup/DependencyResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.Pratices.Unity;
+//using SpaUserControl.Business.Services;
+using SpaUserControl.Domain.Contracts.Repositories;
+//using SpaUserControl.Domain.Contracts.Services;
+using SpaUserControl.Domain.Models;
+using SpaUserControl.Infraestructure.Data;
+using SpaUserControl.Infraestructure.Repositories;
+using Unity;
+using Unity.Lifetime;
+
+namespace SpaUserControl.Startup
+{
+    public static class DependencyResolver
+    {
+        public static void Resolve(UnityContainer container)
+        {
+            container.RegisterType<AppDataContext, AppDataContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserRepository, UserRepository>(new HierarchicalLifetimeManager());
+            //container.RegisterType<IUserService, UserService>(new HierarchicalLifetimeManager());
+
+            container.RegisterType<User, User>(new HierarchicalLifetimeManager());
+        }
+    }
+}
