1ebdb58e3948dd803a2c912611546f530d19a2d5
Leonardo Davinc <davinc131@hotmail.com>
2019-07-20 09:54:08 -0300

    Criação da camada de Business.

---
+++ SpaUserControl.Business/Services/UserService.cs
@@ -0,0 +1,59 @@
+﻿using SpaUserControl.Domain.Contracts.Repositories;
+using SpaUserControl.Domain.Contracts.Services;
+using SpaUserControl.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaUserControl.Business.Services
+{
+    public class UserService : IUserService
+    {
+        private IUserRepository _repository;
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+        public User Authenticate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeInformation(string email, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangePassword(string email, string password, string newPassword, string confirmNewPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetByRange(int skip, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(string name, string email, string password, string confirmPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ResetPassword(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _repository.Dispose();
+        }
+    }
+}
---
+++ SpaUserControl.Domain/Contracts/Services/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using SpaUserControl.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Gerencia os serviços referentes a classe de usuário. Todos os serviços como verificação, autenticação etc. 
+/// </summary>
+namespace SpaUserControl.Domain.Contracts.Services
+{
+    public interface IUserService: IDisposable
+    {
+        User Authenticate(string email, string password);
+        User GetByEmail(string email);
+        void Register(string name, string email, string password, string confirmPassword);
+        void ChangeInformation(string email, string name);
+        void ChangePassword(string email, string password, string newPassword, string confirmNewPassword);
+        string ResetPassword(string email);
+        List<User> GetByRange(int skip, int take);
+    }
+}
